3911f38e177f5cec49b90f958995642cef20dc82
dev-library <dev.rary@gmail.com>
2023-04-26 01:35:56 +0900

    Auditorium 레포지토리에 대한 insert, read 테스트코드 작성완료.

---
+++ src/main/java/kr/co/ticketgacha/domain/auditorium/AutitoriumRepository.java
@@ -0,0 +1,6 @@
+package kr.co.ticketgacha.domain.auditorium;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AutitoriumRepository extends JpaRepository<Auditorium, Long> {
+}
---
+++ src/test/java/kr/co/ticketgacha/domain/AuditoriumTest.java
@@ -0,0 +1,57 @@
+package kr.co.ticketgacha.domain;
+
+import kr.co.ticketgacha.domain.auditorium.Auditorium;
+import kr.co.ticketgacha.domain.auditorium.AutitoriumRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@SpringBootTest
+public class AuditoriumTest {
+
+    @Autowired
+    AutitoriumRepository auditoriumRepository;
+
+    @BeforeEach
+    public void 디비_리셋(){
+        auditoriumRepository.deleteAll();
+    }
+
+    @Test
+    public void 공연장_생성_삭제(){
+
+        // given
+        String name = "테스트공연";
+        int rowCount = 5;
+        int colCount = 10;
+        boolean deleted = false;
+        LocalDateTime createdAt = LocalDateTime.now();
+        LocalDateTime updatedAt = LocalDateTime.now();
+
+        auditoriumRepository.save(Auditorium.builder()
+                                            .name(name)
+                                            .rowCount(rowCount)
+                                            .colCount(colCount)
+                                            .deleted(deleted)
+                                            .createdAt(createdAt)
+                                            .updatedAt(updatedAt)
+                                            .build());
+
+        // when
+        List<Auditorium> auditoriumList = auditoriumRepository.findAll();
+
+        // then
+        Auditorium auditorium = auditoriumList.get(0);
+        Assertions.assertEquals(auditorium.getName(), name);
+        Assertions.assertEquals(auditorium.getRowCount(), rowCount);
+        Assertions.assertEquals(auditorium.getColCount(), colCount);
+        Assertions.assertEquals(auditorium.getUpdatedAt(), updatedAt);
+        Assertions.assertEquals(auditorium.getCreatedAt(), createdAt);
+
+    }
+}
