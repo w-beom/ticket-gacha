52e409e82b4fa07eae5292ca7317c0e6971221b3
w-beom <w_bum@naver.com>
2023-04-26 01:19:43 +0900

    feat : 회원관련기능 추가
    - MemberNotFoundException 추가

---
+++ src/main/java/kr/co/ticketgacha/presentation/exception/MemberNotFoundException.java
@@ -0,0 +1,23 @@
+package kr.co.ticketgacha.presentation.exception;
+
+public class MemberNotFoundException extends RuntimeException{
+    public MemberNotFoundException() {
+        super();
+    }
+
+    public MemberNotFoundException(String message) {
+        super(message);
+    }
+
+    public MemberNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public MemberNotFoundException(Throwable cause) {
+        super(cause);
+    }
+
+    protected MemberNotFoundException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
